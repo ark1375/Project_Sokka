b27dd205bcd7995f459ae5f415424170ae069f03
ark1375 <ark.blackhat@gmail.com>
2020-12-18 18:29:54 +0330

    Labeling critical verticies by +1 and -1 values
    It's still on the progress as the program is having trouble detecting
    the positve and nagetive side of a vector

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -31,6 +31,7 @@
 		 * 
 		 * return counter;
 		 */
+
 import java.applet.Applet;
 import java.awt.Canvas;
 import java.awt.Graphics;
@@ -44,6 +45,9 @@ import java.awt.geom.Point2D.Double;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
+
+import java.lang.Math;
+import java.util.Arrays;
 public final class Main extends JFrame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -67,10 +71,30 @@ public final class Main extends JFrame {
 		  Point2D.Double v16=new Double(473, 262);
 		  Point2D.Double v17=new Double(476, 356);
 		  Point2D.Double x=new Double(502,421);
+
 		  ArrayList<Point2D.Double> p=new ArrayList<Point2D.Double>();
-		  p.add(v1); p.add(v2); p.add(v3); p.add(v4); p.add(v5);p.add(v6);p.add(v7);p.add(v8);
-		  p.add(v9);p.add(v10);p.add(v11);p.add(v12);p.add(v13);p.add(v14);p.add(v15);p.add(v16);p.add(v17);
+		  p.add(v1);
+		  p.add(v2);
+		  p.add(v3);
+		  p.add(v4);
+		  p.add(v5);
+		  p.add(v6);
+		  p.add(v7);
+		  p.add(v8);
+		  p.add(v9);
+		  p.add(v10);
+		  p.add(v11);
+		  p.add(v12);
+		  p.add(v13);
+		  p.add(v14);
+		  p.add(v15);
+		  p.add(v16);
+		  p.add(v17);
+
 		 ArrayList<Point2D.Double> criticalVertecies=new ArrayList<Point2D.Double>();
+
+
+		//checking if the first and last nodes are count as critical vertecies
 		 if(isCritical(x, p.get(0), p.get(p.size()-1), p.get(1)))
 		 {
 			 criticalVertecies.add(p.get(0));
@@ -78,8 +102,8 @@ public final class Main extends JFrame {
 		 if(isCritical(x, p.get(p.size()-1), p.get(p.size()-2), p.get(0)))
 		 {
 			 criticalVertecies.add(p.get(p.size()));
-	
 		 }
+
 		 for(int i=1;i<p.size()-1;i++)
 		 {
 			if(isCritical(x, p.get(i), p.get(i-1), p.get(i+1)))
@@ -87,6 +111,10 @@ public final class Main extends JFrame {
 				criticalVertecies.add(p.get(i));
 			}
 		 }
+
+		 ArrayList<Integer> labels = label_vertices(p, x , criticalVertecies);
+		 System.out.println(Arrays.toString(labels.toArray()));
+
 		 DrawPolygon dp=new DrawPolygon(p,criticalVertecies,x);
 		 dp.main(p,criticalVertecies,x);
 		 for(int i=0;i<criticalVertecies.size();i++)
@@ -95,13 +123,15 @@ public final class Main extends JFrame {
 		 }
 		//intersectionPoints(p, v2, x);
 	}
+
 	public static boolean isCritical(Point2D.Double transmitterLocation,Point2D.Double currentVertex,Point2D.Double previousVertex,Point2D.Double nextVertex)
 	{
 		double slope=(currentVertex.y-transmitterLocation.y)/(currentVertex.x-transmitterLocation.x);   //calculating slope of xv_i vector 
-		double previousVertexHalfPlane=previousVertex.y-currentVertex.y-slope*(previousVertex.x-currentVertex.x); ///calculating if v_i-1 is on + or - half-plane
-		double nextVertexHalfPlane=nextVertex.y-currentVertex.y-slope*(nextVertex.x-currentVertex.x);    ///////calculating if v_i+1 is on + or - half-plane
+		double previousVertexHalfPlane = previousVertex.y - currentVertex.y - slope * (previousVertex.x - currentVertex.x); ///calculating if v_i-1 is on + or - half-plane
+		double nextVertexHalfPlane=nextVertex.y-currentVertex.y - slope*(nextVertex.x-currentVertex.x);    ///////calculating if v_i+1 is on + or - half-plane
 		return (previousVertexHalfPlane*nextVertexHalfPlane>0);		///it means both were above or below half-plane and therefore critical 
 	}
+
 	public static ArrayList<Point2D.Double> intersectionPoints(ArrayList<Point2D.Double> p,Point2D.Double criticalVertex,Point2D.Double x)
 	{
 		
@@ -163,6 +193,114 @@ public final class Main extends JFrame {
 		}
 		
 		return validIntersections;
+	}
+                                     
+	public static ArrayList<Integer> label_vertices(
+								ArrayList<Point2D.Double> polygon ,
+								 	Point2D.Double trmCord ,
+									ArrayList<Point2D.Double> criticalVertecies
+								){
+		ArrayList<Integer> labels = new ArrayList<Integer>();
+
+		for ( int i=0 ; i < criticalVertecies.size() ; i++){
+
+			/* Hypothesis:
+				In this part we are determining if the v_i-1, v_i, v_i+1 is a right-hand or left-hand corner.
+				I suspect that considering the angle that this corner makes if the sin of this angle is 0 or positive,
+				this is a left-turn corner and if the sin is 0 or negative, the corner is a right-hand corner.
+
+				For that we use the law of cosin
+				Note: This didn't work because the corners with the angle greater than 180d are R-h but we cannot make a triangle with
+						an angle greater than 180d
+
+				Hypothesis 2: Using a linear equation we can determin if a point is above or below a line, so usign v_i and v_i-1
+				we create a linear equation (y = mx+b) and turn it into an inequality (y-mx-b <= 0 for the point's bellow the line
+				and y-mx-b >= 0 for the points above the line) using this inequlity we can determine if the third point is above or
+				bellow the line
+
+				Hypothesis 3: We can use the cross-product of the vectors v_i-1,v_i and v_i,v_i+1. This should indicate that the second
+				vector is on the right hand of the first vector (an RH turn) or in the left land of the first vector (a LH turn) 
+				Note that instead of the cross product we can also use the determinant of the below matrix
+			    	   _                   _ 
+					  | vec_1.x   vec_2.x  |  
+			   	  det|	vec_1.y   vec_2.y |
+					|_                  _|
+
+			*/
+
+			/* Report:
+				It detects the left turn and the righ turn accuratly
+			*/
+			
+			int indx_pre_crit_vert , indx_post_crit_vert;
+			int indx_crit_vert = polygon.indexOf( criticalVertecies.get(i) );
+
+			// Because the suitable ds for saving the nodes is a circular array and we have only normal array,
+			// we check the end and the beggining node manualy (as they need to be compared to each other)
+
+			if (indx_crit_vert == 0){
+				indx_post_crit_vert = 1;
+				indx_pre_crit_vert = polygon.size() - 1;
+			} 
+			
+			else if (indx_crit_vert == polygon.size() -1){
+				indx_post_crit_vert = 0;
+				indx_pre_crit_vert = polygon.size() - 2;
+			}
+
+			else {
+				indx_post_crit_vert = indx_crit_vert-1;
+				indx_pre_crit_vert = indx_crit_vert+1;
+			}
+			
+			Point2D.Double vector_1 = new Point2D.Double(
+				polygon.get(indx_pre_crit_vert).x - polygon.get(indx_crit_vert).x, 
+				polygon.get(indx_pre_crit_vert).y - polygon.get(indx_crit_vert).y
+				);
+
+			Point2D.Double vector_2 = new Point2D.Double(
+				polygon.get(indx_crit_vert).x - polygon.get(indx_post_crit_vert).x, 
+				polygon.get(indx_crit_vert).y - polygon.get(indx_post_crit_vert).y
+				);
+				
+			double determinant = (vector_1.x * vector_2.y) - (vector_1.y * vector_2.x); 
+
+			// The left hand corner
+			if (determinant >= 0){
+				double slope = (criticalVertecies.get(i).y - trmCord.y) / (criticalVertecies.get(i).x - trmCord.x);
+				double equation_value = (polygon.get(indx_pre_crit_vert).y - trmCord.y) - (slope*(polygon.get(indx_pre_crit_vert).x - trmCord.x));
+				
+				// V_i-1 is on the negative side of ray
+				if (equation_value <= 0)
+					labels.add(1);
+				else
+					labels.add(-1);
+
+			}
+			
+			// The right-hand corner
+			else{
+				double slope = (criticalVertecies.get(i).y - trmCord.y) / (criticalVertecies.get(i).x - trmCord.x);
+				double equation_value = (polygon.get(indx_pre_crit_vert).y - trmCord.y) - (slope*(polygon.get(indx_pre_crit_vert).x - trmCord.x));
+				
+				// V_i-1 is on the negative side of ray
+				if (equation_value <= 0)
+					labels.add(-1);
+				else
+					labels.add(1);
+
+			}
+				
+		}
+
+		return labels;
+
+	}
+
+	public static double distance(Point2D.Double a , Point2D.Double b){
+
+		return Math.sqrt( Math.pow(a.x - b.x , 2) + Math.pow(a.y - b.y , 2));
+
 	}
 
 }
